70980bc36a1868f32d5a19105b285e569f1bc0b1
Kevin Campos <kesc23@hotmail.com>
2025-03-04 02:15:42 -0300

    add listCampaigns

--- src/controllers/campaign-controller.ts
+++ src/controllers/campaign-controller.ts
@@ -12,6 +12,39 @@ class CampaignController {
         return res.status(500).json({ errors: ['something went wrong'] });
     }
 
+    static async listCampaigns(req: Request, res: Response) {
+        const model = req.model as Campaign;
+        const { page, limit } = req.query;
+        
+        const errors: string[] = [];
+
+        if(CampaignController.returnErrorIfModelNotDefined(req, res)) return;
+
+        if(limit && !isStringParseableToInt(limit as string)) {
+            errors.push("limit must be a valid number"); 
+        }
+
+        if(page) {
+            if (isStringParseableToInt(page as string) && parseInt(page as string) < 1) {
+                errors.push("page must be greater than or equal to 1");
+            }
+            if (!isStringParseableToInt(page as string)) errors.push("page must be a valid number");
+        }
+
+
+        if(errors.length > 0) {
+            res.status(400).json({ errors });
+            return;
+        }
+        
+        const units = parseInt(limit as string) || 20;
+        const offset = parseInt(page as string) * units - units || 0;
+
+        const campaigns = await model.getCampaigns(units, offset);
+
+        res.status(200).json({ campaigns });
+    }
+
 }
 
 export default CampaignController;
